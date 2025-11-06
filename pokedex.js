1b5fd9541b0298cf60cc9498edb58e4cee585fdb
Rami Nadif <s2401205@edu.bc.fi>
2025-11-06 12:52:58 +0200

    moi

--- pokedex.js
+++ pokedex.js
@@ -1,18 +1,26 @@
 async function info() {
-    try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=2000")
+  try {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=2000"
+    );
 
-        const data = await response.json()
-        const pokemons = data.slice(0,1000)
-        const pokemoninfo = document.querySelector(".main")
-        pokemoninfo.innerHTML =""
+    const data = await response.json();
+    const pokemons = data.results.slice(0, 1328);
+    const pokemoninfo = document.getElementById("box");
+    pokemoninfo.innerHTML = "";
 
-        pokemons.foreach((pokemon)) => {
-            const item1 = document.createElement("div")
-            const item2 = document.createElement("p")
-            const name = pokemon.name
-            const url = pokemon.url
-        }
-
-    }   
+    pokemons.forEach((pokemon) => {
+      const item1 = document.createElement("div");
+      const item2 = document.createElement("p");
+      const name = pokemon.name;
+      const url = pokemon.url;
+      item1.textContent = name;
+      item2.textContent = url;
+      item1.appendChild(item2);
+      pokemoninfo.appendChild(item1);
+    });
+  } catch (error) {
+    console.error("Virhe");
+  }
 }
+info();
