8c1c1084ce5a9255a8092d80ab1c77e77227bb00
Miiko Manula <S2401341@edu.bc.fi>
2025-11-10 12:01:43 +0200

    info sivu testi

---
+++ info.js
@@ -0,0 +1,50 @@
+// Haetaan URL-parametri 'id', joka kertoo mikä Pokémon valittiin
+// Esim jos käyttäjä klikkaa Bulbasauria, URL on info.html?git
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+// Funktio, joka hakee Pokémonin tiedot PokeAPI:sta
+async function loadPokemon() {
+  try {
+    // Haetaan JSON-data API:sta käyttäen id:tä
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await res.json();
+
+    // Haetaan container-elementti, johon tiedot rendataan
+    const container = document.getElementById("pokemon-info");
+
+    // Haetaan front ja back spritet (kuvat)
+    const frontSprite = data.sprites.front_default;
+    const backSprite = data.sprites.back_default;
+
+    // Haetaan Pokémonin tyypit listaksi
+    const types = data.types.map((t) => t.type.name);
+
+    // rendataan tiedot html:ksi containeriin
+    container.innerHTML = `
+      <h2 class="pokemon-name">${data.name.toUpperCase()}</h2>
+      <div class="sprites">
+        <!-- Front sprite aina -->
+        <img src="${frontSprite}" alt="${data.name} front" class="sprite">
+        <!-- Back sprite vain jos saatavilla -->
+        ${
+          backSprite
+            ? `<img src="${backSprite}" alt="${data.name} back" class="sprite">`
+            : ""
+        }
+      </div>
+      <div class="details">
+        <p><b>Height:</b> ${data.height}</p>
+        <p><b>Weight:</b> ${data.weight}</p>
+        <p><b>Types:</b> ${types.join(", ")}</p>
+      </div>
+    `;
+  } catch (err) {
+    // Virheen käsittely, jos API haku ei wörki
+    document.getElementById("pokemon-info").innerHTML =
+      "Pokémonia ei löytynyt 😢";
+    console.error("Virhe haettaessa Pokémonia:", err);
+  }
+}
+
+loadPokemon();
